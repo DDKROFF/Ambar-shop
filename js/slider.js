521c09c5101a1a8f2f9e078aae58fac908a14a44
DDKROFF <ddkroff@gmail.com>
2025-06-25 16:19:24 +0300

    Первый коммит а так же шапка

---
+++ js/slider.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const slider = document.querySelector('.slider');
+  const container = document.querySelector('.slider__container');
+  const slideWidth = 342.33;
+  let offset = 0;
+  const speed = 0.5;
+  const slides = slider.querySelectorAll('a');
+  for (let i = 0; i < 2; i++) {
+    const clone = slides[i].cloneNode(true);
+    slider.appendChild(clone);
+  }
+
+  function animate() {
+    offset += speed;
+
+    if (offset >= slideWidth) {
+      offset -= slideWidth;
+      const firstSlide = slider.querySelector('a');
+      slider.appendChild(firstSlide);
+    }
+
+    slider.style.transform = `translateX(${-offset}px)`;
+    requestAnimationFrame(animate);
+  }
+
+  //Для адаптивности слайдера
+  function updateContainerWidth() {
+    const maxWidth = 1850;
+    const sidePadding = 80;
+    const availableWidth = Math.min(window.innerWidth, maxWidth + sidePadding) - sidePadding;
+    container.style.maxWidth = `${availableWidth}px`;
+  }
+
+  window.addEventListener('resize', updateContainerWidth);
+  updateContainerWidth();
+
+  animate();
+});
